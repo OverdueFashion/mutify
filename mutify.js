cfe74b5c24ceb23f304145b5ef3b9620438ad968
Goddamnnoob <gowthamreddy547@gmail.com>
2021-01-31 17:04:29 +0530

    Inefficient

--- mutify.js
+++ mutify.js
@@ -1,15 +1,36 @@
-window.onload = function(){ d = document.querySelector("#main > div > div.Root__top-container > div.Root__now-playing-bar > footer > div > div.now-playing-bar__center > div > div.playback-bar > div:nth-child(3)").innerText; // current song's duration
-d = d.split(":");
-d = Math.floor(d[0])*60 + Math.floor(d[1]);
-console.log(d);
-alert(d)
-var popo = $("#main > div > div.Root__top-container > div.Root__now-playing-bar > footer > div > div.now-playing-bar__center > div > div.playback-bar > div:nth-child(3)");
-$("#main > div > div.Root__top-container > div.Root__now-playing-bar > footer > div > div.now-playing-bar__center > div > div.playback-bar > div:nth-child(3)").bind("DOMSubtreeModified",function(){
-    i = popo.split(':');
-    i = Math.floor(i[0])*60 + Math.floor(i[1]);
-    console.log(i);
-    if (i > 30){
-        
-    }
+window.onload = setTimeout(() => {
+    const d = document.querySelector("#main > div > div.Root__top-container > div.Root__now-playing-bar > footer > div > div.now-playing-bar__center > div > div.playback-bar > div:nth-child(3)"); // current song's duration
+    const mute = document.querySelector("#main > div > div.Root__top-container > div.Root__now-playing-bar > footer > div > div.now-playing-bar__right > div > div.volume-bar > button"); //mute button element
+    setInterval(() => {
+        dur = d.innerText.split(":");
+        dur = Math.floor(dur[0])*60 + Math.floor(dur[1]);
+        console.log(dur);
+        if(dur>30){
+            if(mute.className == "spoticon-volume-off-16 control-button volume-bar__icon"){
+                mute.click();
+            }
+        }
+        else if (dur == 0 ){
+            //No song 
+        }
+        else {
+            if(mute.className == "spoticon-volume-twowave-16 control-button volume-bar__icon"){
+                mute.click();
+            }
+        }
+    }, 3000);
+   
+}, 5000); 
+/*
     console.log('changed');
-});}
+     var observer = new MutationObserver(function(mutations) {
+        mutations.forEach(function(mutation) {
+            console.log("changed");
+        });
+    });
+    
+    var config = { attributes: true, childList: true, characterData: true }
+    observer.observe(d, config);
+    observer.disconnect();
+
+}); }*/
